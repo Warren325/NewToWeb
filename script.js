89b7ffe5ccf455838dcae517dc5a5c1a92cb10bb
Warren325 <121258014+Warren325@users.noreply.github.com>
2026-01-13 23:04:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section, footer");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
